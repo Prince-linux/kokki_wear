c7af2a968802a285a3b63e54de217f7cc728ca25
Prince Oppong Boamah <peedor@Princes-MacBook-Pro.local>
2019-12-18 10:36:04 +0000

    Prince: initial commit

---
+++ cart/cart.py
@@ -0,0 +1,18 @@
+from decimal import Decimal
+
+from django.conf import settings
+from shop.models import Product
+
+class Cart(object):
+
+    def __init__(self, request):
+        """
+        initalise the cart
+        """
+        self.session = request.session
+        cart = self.session.get(settings.CART_SESSION_ID)
+
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] = {}
+        self.cart = cart
+
