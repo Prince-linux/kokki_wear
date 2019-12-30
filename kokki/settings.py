45d95907e4b79285d754106b07932a590a61ba8b
peedor <user@Users-MacBook-Air.local>
2019-12-30 11:34:34 +0000

    Prince: Fixed broken files

--- kokki/settings.py
+++ kokki/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
-    'cart.apps.CartConfiq',
+    'cart.apps.CartConfig',
     
 ]
 
